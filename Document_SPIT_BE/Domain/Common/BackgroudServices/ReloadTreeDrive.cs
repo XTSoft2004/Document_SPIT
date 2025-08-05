8826cb13147f83c202a3edb259b5ea0aec37b85e
Tran Xuan Truong <tranxuantruong420@gmail.com>
2025-08-05 20:50:58 +0700

    [UPDATE] - Update check token drive

--- Document_SPIT_BE/Domain/Common/BackgroudServices/ReloadTreeDrive.cs
+++ Document_SPIT_BE/Domain/Common/BackgroudServices/ReloadTreeDrive.cs
@@ -6,6 +6,7 @@ using Domain.Interfaces.Database;
 using Domain.Interfaces.Repositories;
 using HelperHttpClient;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -16,6 +17,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Domain.Common.BackgroudServices
@@ -58,7 +60,9 @@ namespace Domain.Common.BackgroudServices
                 scope.ServiceProvider.GetRequiredService<IHttpContextAccessor>();
                 string? nextPageToken = null;
 
+                await GetAccessToken(); // Lấy access token nếu cần thiết
                 _request.SetAuthentication(TokenDriverStore.Access_Token);
+
                 do
                 {
                     var url = $"https://www.googleapis.com/drive/v3/files?pageSize=1000" +
@@ -132,6 +136,46 @@ namespace Domain.Common.BackgroudServices
                 AppExtension.PrintWithRandomColor($"Đã tải lại danh sách file từ Google Drive, tổng số file: {files.Count}"); // In ra thông báo lỗi với màu ngẫu nhiên
             }
         }
+        public async Task<string> GetAccessToken()
+        {
+            using var scope = _scopeFactory.CreateScope();
+            IConfiguration _config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+            TokenInfoGoogleResponse InfoToken = await GetInfoToken(TokenDriverStore.Access_Token);
 
+            if (InfoToken == null || (InfoToken != null && Convert.ToInt32(InfoToken.exp) <= DateTimeOffset.UtcNow.ToUnixTimeSeconds()))
+            {
+                var JsonData = new Dictionary<string, string>
+                {
+                    { "client_id", _config["GoogleInfo:client_id"] },
+                    { "client_secret", _config["GoogleInfo:client_secret"] },
+                    { "refresh_token", _config["GoogleInfo:refresh_token"] },
+                    { "grant_type", "refresh_token" }
+                };
+                var response = await _request.PostAsync("https://oauth2.googleapis.com/token", JsonData);
+                if (response.IsSuccessStatusCode)
+                {
+                    string access_token = Regex.Match(_request.Content, "\"access_token\": \"(.*?)\"").Groups[1].Value;
+                    TokenDriverStore.Access_Token = access_token;
+                    _request.SetAuthentication(TokenDriverStore.Access_Token);
+                    return access_token;
+                }
+            }
+            return TokenDriverStore.Access_Token;
+        }
+        private async Task<TokenInfoGoogleResponse> GetInfoToken(string access_token)
+        {
+            if (string.IsNullOrEmpty(access_token))
+                return null;
+            var response = await _request.GetAsync($"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = JsonConvert.DeserializeObject<TokenInfoGoogleResponse>(_request.Content);
+                return jsonData;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
